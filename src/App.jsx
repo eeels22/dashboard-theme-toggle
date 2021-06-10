6af00a6b51735485f9e2ff7684cdb0d63d45990f
En-Chi Liu <enchi.liu@gmail.com>
2021-06-11 00:35:55 +0200

    Implement Header compnent and App

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,34 @@
 //Project files
 import "./styles/style.css";
+import Header from "./components/Header";
+import OverviewList from "./components/OverviewList";
+import toplineData from "./data/topline.json";
+import FollowerCard from "./components/FollowerCard";
 
 function App() {
-  return <div className="App"></div>;
+  // Constants
+  const followerCardArray = toplineData.map((item) => (
+    <FollowerCard key={item.id} info={item} />
+  ));
+
+  return (
+    <div className="App">
+      <Header />
+      <section>{followerCardArray}</section>
+      <OverviewList />
+      <footer class="attribution">
+        Challenge by{" "}
+        <a
+          href="https://www.frontendmentor.io?ref=challenge"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Frontend Mentor
+        </a>
+        . Coded by <a href="https://github.com/eeels22">En-Chi Liu</a>.
+      </footer>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+// Project files
+import ThemeToggler from "./ThemeToggler";
+
+export default function Header() {
+  return (
+    <header>
+      <h1>Social Media Dashboard</h1>
+      <p>Total Followers: 23,004</p>
+      <hr />
+      <ThemeToggler />
+    </header>
+  );
+}
