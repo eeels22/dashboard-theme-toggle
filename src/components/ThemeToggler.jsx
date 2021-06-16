113699aadffd55766bb023254edcbfc6de9119e7
En-Chi Liu <enchi.liu@gmail.com>
2021-06-16 12:23:59 +0200

    style button

--- src/components/ThemeToggler.jsx
+++ src/components/ThemeToggler.jsx
@@ -1,17 +1,41 @@
 export default function ThemeToggler() {
   return (
     <div className="ThemeToggler">
-      <span className="sub-heading" id="dark-mode">
+      <button className="toggle" type="button" aria-pressed="false">
         Dark mode
-      </span>
-      <button
-        id="button-dark-mode"
-        role="switch"
-        aria-checked="true"
-        aria-labelledby="dark-mode"
-      >
-        <span>On</span>
-        <span>Off</span>
+        <span className="toggle-display" hidden>
+          <svg
+            width="18"
+            height="14"
+            viewBox="0 0 18 14"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+            aria-hidden="true"
+            focusable="false"
+            className="toggle-icon toggle-icon-checkmark"
+          >
+            <path
+              d="M6.08471 10.6237L2.29164 6.83059L1 8.11313L6.08471 13.1978L17 2.28255L15.7175 1L6.08471 10.6237Z"
+              fill="currentcolor"
+              stroke="currentcolor"
+            />
+          </svg>
+          <svg
+            width="13"
+            height="13"
+            viewBox="0 0 13 13"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+            aria-hidden="true"
+            focusable="false"
+            className="toggle-icon toggle-icon-cross"
+          >
+            <path
+              d="M11.167 0L6.5 4.667L1.833 0L0 1.833L4.667 6.5L0 11.167L1.833 13L6.5 8.333L11.167 13L13 11.167L8.333 6.5L13 1.833L11.167 0Z"
+              fill="currentcolor"
+            />
+          </svg>
+        </span>
       </button>
     </div>
   );
